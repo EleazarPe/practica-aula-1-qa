c8305c75d1b6816907bfc4e7140941a53705e37c
Carlos E. Peterson <71235444+EleazarPe@users.noreply.github.com>
2025-05-27 19:51:10 -0400

    feat(llenando empleados): llenando empleado por test

--- src/test/java/ServivioNominaTest.java
+++ src/test/java/ServivioNominaTest.java
@@ -3,6 +3,7 @@ import org.example.ServicioNomina;
 import org.example.TipoEmpleado;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 
 import java.math.BigDecimal;
 
@@ -11,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ServivioNominaTest {
 
     private  Empleado crearEmpleadoTest() {
-        Empleado empleado = new Empleado(1l, TipoEmpleado.FULL_TIME, new BigDecimal("40"), new BigDecimal("10"), new BigDecimal("20.00"));
+        Empleado empleado = new Empleado(1l, TipoEmpleado.PART_TIME, new BigDecimal("40"), new BigDecimal("10"), new BigDecimal("20.00"));
         return empleado;
     }
 
@@ -27,6 +28,13 @@ public class ServivioNominaTest {
     @Test
     public void calcularPagoHorasExtraTest(){
         Empleado empleado = crearEmpleadoTest();
-        assertEquals(new BigDecimal("200.00"), empleado.getHorasExtra().multiply(empleado.getTarifaxHora()));
+        assertEquals(new BigDecimal("300.00"), ServicioNomina.calcularPagoHorasExtra(empleado).setScale(2, BigDecimal.ROUND_HALF_UP));
+    }
+    @Test
+    public void aplicarBonoTest(){
+        Empleado empleado = crearEmpleadoTest();
+
+        assertEquals(new BigDecimal("500.00"), ServicioNomina.calcularAplicarBono(empleado).setScale(2, BigDecimal.ROUND_HALF_UP));
+
     }
 }
