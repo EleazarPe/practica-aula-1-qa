e98307e7680a426e2be071d07bbcc5f98eee1528
Carlos E. Peterson <71235444+EleazarPe@users.noreply.github.com>
2025-05-27 18:54:12 -0400

    primer commit

---
+++ src/main/java/org/example/Empleado.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Empleado {
+    private Long id;
+
+    private TipoEmpleado tipoEmpleado;
+    private BigDecimal horasTrabajadas;
+    private BigDecimal horasExtra;
+    private BigDecimal tarifaxHora;
+}
