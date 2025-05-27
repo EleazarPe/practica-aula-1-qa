1f141116b0cc8dd9aca83c62d5bccbdf72ed3381
miguel <20190327@ce.pucmm.edu.do>
2025-05-27 19:10:06 -0400

    pago base test

--- src/main/java/org/example/ServicioNomina.java
+++ src/main/java/org/example/ServicioNomina.java
@@ -1,4 +1,12 @@
 package org.example;
 
+import java.math.BigDecimal;
+
 public class ServicioNomina {
+
+    public BigDecimal pagoBaseCalc(Empleado empleado) {
+
+        return empleado.getTarifaxHora().multiply(empleado.getHorasTrabajadas());
+
+    }
 }
--- src/test/java/ServivioNominaTest.java
+++ src/test/java/ServivioNominaTest.java
@@ -1,2 +1,20 @@
+import org.example.Empleado;
+import org.example.ServicioNomina;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ServivioNominaTest {
+
+    @Test
+    public void pagoBaseTest(){
+        ServicioNomina servicioNomina = new ServicioNomina();
+
+        Empleado empleado = new Empleado();
+
+        assertEquals(new BigDecimal(40 * 10), servicioNomina.pagoBaseCalc(empleado));
+    }
+
 }
